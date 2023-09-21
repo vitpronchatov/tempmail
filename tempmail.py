94c7530b85ed9c2f2fa1bd4364adad6a75b60702
Vitaliy Pronchatov <vitalypronchatov@gmail.com>
2023-09-21 14:05:08 +0300

    Initial commit

--- tempmail.py
+++ tempmail.py
@@ -2,88 +2,10 @@ import sys
 import requests
 import json
 import time
-
-
-class Mail:
-
-    def __init__(self, m=None):
-
-        """Процедура инициализации"""
-
-        a = "https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=1"
-        if m is None:
-            self.mail = json.loads(requests.post(a).text)[0]
-        else:
-            self.mail = m
-        print(self.mail)
-
-    def check_messages(self):
-
-        """Процедура проверки входящего сообщения"""
-
-        a = self.mail.split('@')
-        b = json.loads(
-            requests.get(f"https://www.1secmail.com/api/v1/?action=getMessages&login={a[0]}&domain={a[1]}").text)
-        lis = []
-        for i in b:
-            lis.append('|Id: ' + str(i["id"]) + "\n" + "|From: " + i["from"] + "\n" + "|Subject: " + i[
-                "subject"] + "\n" + "|Date: " + i["date"])
-        return "\n\n".join(lis)
-
-    def get_message_id(self):
-
-        """Получение идентификатора сообщения"""
-
-        a = self.mail.split('@')
-        b = json.loads(
-            requests.get(f"https://www.1secmail.com/api/v1/?action=getMessages&login={a[0]}&domain={a[1]}").text)
-        try:
-            return b[0]['id']
-        except:
-            print("No messages!")
-
-    def get_message_subject(self):
-
-        """Получение заголовка сообщения"""
-
-        a = self.mail.split('@')
-        b = json.loads(
-            requests.get(f"https://www.1secmail.com/api/v1/?action=getMessages&login={a[0]}&domain={a[1]}").text)
-        try:
-            return b[0]['subject']
-        except:
-            print("No messages!")
-
-    def get_message_by_id(self, id):
-
-        """Процедура открытия конкретного сообщения (по айди)"""
-
-        b = self.mail.split('@')
-        d = json.loads(requests.get(
-            f"https://www.1secmail.com/api/v1/?action=readMessage&login={b[0]}&domain={b[1]}&id={id}").text)
-        html = d["htmlBody"]
-        body = d["body"]
-        if len(html) > 0:
-            html = d["htmlBody"]
-        else:
-            html = "None"
-        attach = d["attachments"]
-        if len(attach) > 0:
-            lis = []
-            attach = d["attachments"]
-            for i in attach:
-                lis.append(
-                    f'https://www.1secmail.com/api/v1/?action=download&login={b[0]}&domain={b[1]}&id={id}&file={i["filename"]}')
-            attach = ", ".join(lis)
-        else:
-            attach = "None"
-        c = '|Id: ' + str(d["id"]) + "\n" + "|From: " + d["from"] + "\n" + "|Subject: " + d[
-            "subject"] + "\n" + "|Date: " + d["date"] + "\n" + "|Body: " + body + "\n|TextBody: " + d[
-                "textBody"] + "\n" + "|HtmlBody: " + html + "\n" + "|Attachments: " + attach
-        return c
-
-    def get_message_email_name(self):
-        return self.mail
+import string
+import random
+from hashlib import md5
+from TempMail import TempMail
 
 
 def check_email(email_name):
@@ -104,66 +26,72 @@ def check_email(email_name):
 url = 'https://hidemy.io/ru/demo/'
 
 if 'Ваша электронная почта' in requests.get(url).text:
-    email = Mail()
-    email_name = email.get_message_email_name()
+    email = TempMail()
+    inbox = TempMail.generateInbox(email)
+    print(inbox.address)
 
-    email_is_valid = check_email(email_name)
+    email_is_valid = check_email(inbox.address)
 
     while email_is_valid is not True:
-        email = Mail()
-        email_name = email.get_message_email_name()
-        email_is_valid = check_email(email_name)
+        email = TempMail()
+        inbox = TempMail.generateInbox(email)
+        email_is_valid = check_email(inbox.address)
 
-    if email_is_valid:
-        print("Ждем 70 секунд...")
-        time.sleep(70)
-        try:
-            message = email.get_message_by_id(email.get_message_id())
-            message_subject = email.get_message_subject()
-        except:
-            print("Ждем еще 20 секунд...")
-            time.sleep(20)
-            try:
-                message = email.get_message_by_id(email.get_message_id())
-                message_subject = email.get_message_subject()
-            except:
-                print("К сожалению получить письмо не получилось...")
-                sys.exit()
-        if message_subject == "Подтвердите e-mail":
-            ver_link = message[message.find('Подтвердить') + 262:message.find('Подтвердить') + 306]
-        else:
-            print("Сообщение не получено...")
-        print("Ссылка для подтверждения e-mail: ")
-        print(ver_link)
+    print("Ждем 70 секунд...")
+    time.sleep(70)
+    message = TempMail.getEmails(email, inbox=inbox)
+    print(message)
 
-        while True:
-            try:
-                response = requests.get(ver_link)
-                if 'Спасибо' in response.text:
-                    print('Почта подтверждена. Код отправлен на вашу почту!')
-                    break
-                else:
-                    ver_link = input('Ссылка невалидная, повторите попытку: ')
-            except:
-                ver_link = input('Ссылка невалидная, повторите попытку: ')
-                continue
-            input("Нажмите Enter для продолжения...")
-
-    print("Получение тестового кода...")
-    time.sleep(10)
-
-    message = email.get_message_by_id(email.get_message_id())
-    print("Ваш тестовый код:")
-    print(message[message.find('Ваш тестовый код: ') + 18:message.find('Ваш тестовый код: ') + 32])
-
-    lines = 0
-    file = open("/content/drive/MyDrive/Colab Notebooks/codes.txt", 'a+')
-    file.write('\n')
-    file.write(message[message.find('Ваш тестовый код: ') + 18:message.find('Ваш тестовый код: ') + 32])
-    with open("/content/drive/MyDrive/Colab Notebooks/codes.txt") as file:
-        for line in file:
-            lines += 1
-        print('Сгенерировано кодов: ' + str(lines))
-    file.close()
+    # if email_is_valid:
+    #     print("Ждем 70 секунд...")
+    #     time.sleep(70)
+    #     try:
+    #         message = TempMail.getEmails(email, inbox=inbox)
+    #         print(message)
+    #      except:
+    #          print("Ждем еще 20 секунд...")
+    #          time.sleep(20)
+    #          try:
+    #              message = TempMail.getEmails(email, inbox=inbox)
+    #              message_subject = email.get_message_subject()
+    #          except:
+    #              print("К сожалению получить письмо не получилось...")
+    #              sys.exit()
+    #     if message_subject == "Подтвердите e-mail":
+    #         ver_link = message[message.find('Подтвердить') + 262:message.find('Подтвердить') + 306]
+    #     else:
+    #         print("Сообщение не получено...")
+    #     print("Ссылка для подтверждения e-mail: ")
+    #     print(ver_link)
+    #
+    #     while True:
+    #         try:
+    #             response = requests.get(ver_link)
+    #             if 'Спасибо' in response.text:
+    #                 print('Почта подтверждена. Код отправлен на вашу почту!')
+    #                 break
+    #             else:
+    #                 ver_link = input('Ссылка невалидная, повторите попытку: ')
+    #         except:
+    #             ver_link = input('Ссылка невалидная, повторите попытку: ')
+    #             continue
+    #         input("Нажмите Enter для продолжения...")
+    #
+    # print("Получение тестового кода...")
+    # time.sleep(10)
+    #
+    # message = email.get_message_by_id(email.get_message_id())
+    # print("Ваш тестовый код:")
+    # print(message[message.find('Ваш тестовый код: ') + 18:message.find('Ваш тестовый код: ') + 32])
+    #
+    # lines = 0
+    # file = open("/content/drive/MyDrive/Colab Notebooks/codes.txt", 'a+')
+    # file.write('\n')
+    # file.write(message[message.find('Ваш тестовый код: ') + 18:message.find('Ваш тестовый код: ') + 32])
+    # with open("/content/drive/MyDrive/Colab Notebooks/codes.txt") as file:
+    #     for line in file:
+    #         lines += 1
+    #     print('Сгенерировано кодов: ' + str(lines))
+    # file.close()
 else:
     print('Невозможно получить тестовый период')
