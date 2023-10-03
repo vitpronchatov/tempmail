2702931428c853bd875bd8f165c2615105dbac31
vitpronchatov <70130118+vitpronchatov@users.noreply.github.com>
2023-10-03 22:43:54 +0300

    Update hmn-generator.py

--- hmn-generator.py
+++ hmn-generator.py
@@ -2,32 +2,32 @@ import requests
 
 url = 'https://hidemy.io/ua/demo/'
 
-if 'Ваша электронная почта' in requests.get(url).text:
+if 'Ваша електронна пошта' in requests.get(url).text:
     
-    email = input('Введите электронную почту для получения тестового периода: ')
+    email = input('Введіть електронну пошту для отримання тестового періода: ')
 
-    response = requests.post('https://hidemy.io/ru/demo/success/', data={
+    response = requests.post('https://hidemy.io/ua/demo/success/', data={
         "demo_mail": f"{email}"
     })
 
-    if 'Ваш код выслан на почту' in response.text:
-        confirm = input('Введите полученную ссылку для подтверждения e-mail адреса: ')
+    if 'Ваш код відправлений на пошту' in response.text:
+        confirm = input('Введіть отримане посилання для підтвердження e-mail адреси: ')
         
         while True:
             try:
                 response = requests.get(confirm)
-                if 'Спасибо' in response.text:
-                    print('Почта подтверждена. Код отправлен на вашу почту!')
+                if 'Дякую' in response.text:
+                    print('Пошта підтверджена. Код відправлений на вашу пошту!')
                     break
                 else:
-                    confirm = input('Ссылка невалидная, повторите попытку: ')
+                    confirm = input('Посилання недійсне, повторіть спробу: ')
             except:
-                confirm = input('Ссылка невалидная, повторите попытку: ')
+                confirm = input('Посилання недійсне, повторіть спробу: ')
                 continue
 
 
     else:
-        print('Указанная почта не подходит для получения тестового периода ')
+        print('Вказана пошта не підходить для отримання тестового періода ')
 
 else:
-    print('Невозможно получить тестовый период')
+    print('Неможливо отримати тестовий період')
